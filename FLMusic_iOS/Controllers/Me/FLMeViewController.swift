2f005f0e40c4d8e2ea13132d538ade7f4fe634cd
fengli <954751186@qq.com>
2018-06-12 09:38:14 +0800

    searchView 新增

---
+++ FLMusic_iOS/Controllers/Me/FLMeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FLMeViewController.swift
+//  FLMusic_iOS
+//
+//  Created by fengli on 2018/6/1.
+//  Copyright © 2018年 fengli. All rights reserved.
+//
+
+import UIKit
+
+class FLMeViewController: FLBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
